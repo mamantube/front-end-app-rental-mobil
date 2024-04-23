fcb38a03c8a1863247b9af2c945023264dc67c1b
mamantube <firmanhiday.id007@gmail.com>
2024-04-23 16:18:09 +0700

    add loading store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { combineReducers, legacy_createStore as createStore } from "redux";
 import { reducerCounter} from "./reducers/counter.js";
+import { reducerLoading } from "./reducers/loading.js";
 
 let store = combineReducers({
     counter: reducerCounter,
+    loading: reducerLoading,
 });
 
 export default createStore(store)
