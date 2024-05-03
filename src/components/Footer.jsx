2fd7444783462ba726421b5f3ce9b51b3e26d97f
mamantube <firmanhiday.id007@gmail.com>
2024-05-03 16:12:58 +0700

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer id="footer--rental--mobil" className="text-s3">
+            Maman Rental Mobil - Copyright 2024
+        </footer>
+    )
+}
