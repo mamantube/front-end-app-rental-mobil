6bb2930ab599b8a8968addc699eff801265f5dc3
mamantube <firmanhiday.id007@gmail.com>
2024-05-03 16:30:50 +0700

    add sm media query for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer id="footer--rental--mobil" className="text-s3">
+        <footer id="footer--rental--mobil">
             Maman Rental Mobil - Copyright 2024
         </footer>
     )
