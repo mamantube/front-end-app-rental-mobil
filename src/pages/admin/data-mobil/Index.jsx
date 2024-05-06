e363dc3c14ff5eeec5471a2b5c1b28d2ee55ede5
mamantube <firmanhiday.id007@gmail.com>
2024-05-06 15:20:47 +0700

    add neavbreadcrumb component

--- src/pages/admin/data-mobil/Index.jsx
+++ src/pages/admin/data-mobil/Index.jsx
@@ -1,7 +1,22 @@
+import NavBreadcrumb from "../../../components/NavBreadcrumb"
+
 export default function DataMobil () {
+    let navList = [
+        {
+            to:"/",
+            title: "Data Mobil",
+            isActive: false
+        },
+        {
+            to:"/admin/data-pengguna",
+            title: "Data Pengguna",
+            isActive: false,
+        }
+    ]
     return (
-        <div className=" min-vh-100">
+        <section id="list--data--mobil" className=" min-vh-100">
+            <NavBreadcrumb navList={navList} />
             <h2>Data Mobil Maman Rental Mobil</h2>
-        </div>
+        </section>
     )
 }
