67abfc7d0290231b666ec1bb6a0010a6c66d630f
mamantube <firmanhiday.id007@gmail.com>
2024-05-10 22:27:43 +0700

    add useAxios

---
+++ src/hooks/useAxios.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+
+export default function useAxios() {
+    const { token } = useSelector((store) => store.user);
+
+    const axiosInstance = axios.create({
+        baseURL: import.meta.env.VITE_BASE_API_URL,
+    });
+
+    axiosInstance.interceptors.request.use(
+        (config) => {
+            if (token) config.headers["Authorization"] = `Bearer ${token}`;
+            console.log("INI", config);
+            return config;
+        }, 
+        (error) => {
+            return Promise.reject(error)
+        });
+
+        axiosInstance.interceptors.response.use(
+            (config) => {
+                return config;
+            },
+            (error) => {
+                const code = error.response.status;
+                
+                if (code === 400) {
+                    let {message} = error.response.data;
+                    toast.error(message)
+                }
+
+                if (code === 401) {
+                    let {message} = error.response.data;
+                    toast.error(message)
+                }
+
+                return Promise.reject(error)
+            }
+        )
+
+    return axiosInstance;
+}
