22d96399d13d9d5faaca929f9f700637480014b4
mamantube <firmanhiday.id007@gmail.com>
2024-05-14 14:22:15 +0700

    create card product component

---
+++ src/components/CardProduct.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import { Card, Button } from "react-bootstrap";
+import { formatIDR } from "../utils/formater";
+
+export default function CardProduct({product = {}, buttonText = "Edit", onClickBtnCard = () => {} }) {
+    return (
+        <Card className=" rounded-0">
+            <Card.Img variant="top" src={product.storage_detail.secure_url} className=" rounded-0"/>
+            <Card.Body className=" text-center">
+                <Card.Title>{product.name}</Card.Title>
+                <Card.Text>{formatIDR(product.price)}</Card.Text>
+
+                <Button variant="dark" className=" rounded-0 w-50" onClick={onClickBtnCard}>
+                    {buttonText}
+                </Button>
+            </Card.Body>
+        </Card>
+    )
+}
--- src/components/admin/data-mobil/ListProduct.jsx
+++ src/components/admin/data-mobil/ListProduct.jsx
@@ -1,30 +1,23 @@
 /* eslint-disable react/prop-types */
-import { Card, Row, Col, Button } from "react-bootstrap";
+import { Row, Col, } from "react-bootstrap";
 import EmptyProduct from "../../EmptyProduct";
-import { formatIDR } from "../../../utils/formater.js";
+import CardProduct from "../../CardProduct";
 
 
 export default function ListProduct({dataProduct = []}) {
     const divStyle = {
-        marginTop: "2.875rem"
-    }
+        marginTop: "2.875rem"}
     if (!dataProduct.length) return  <EmptyProduct />
+
+    const onEditProduct = (product) => {
+        console.log("ini", product)
+    }
     return (
         <div style={divStyle} className=" min-vh-100">
             <Row>
                 {dataProduct.map((product, index) => (
                     <Col key={`card-product-${index + 1}`} lg="3" >
-                        <Card key={`card-product-${index + 1}`} className=" rounded-0">
-                            <Card.Img variant="top" src={product.storage_detail.secure_url} className=" rounded-0"/>
-                            <Card.Body className=" text-center">
-                                <Card.Title>{product.name}</Card.Title>
-                                <Card.Text>{formatIDR(product.price)}</Card.Text>
-
-                                <Button variant="dark" className=" rounded-0 w-50">
-                                    Edit
-                                </Button>
-                            </Card.Body>
-                        </Card>
+                        <CardProduct product={product} onClickBtnCard={() => onEditProduct(product)}/>      
                     </Col>
                 ))}
             </Row>
