3143ef3355777f00205a91c26ef207803ec78eb3
mamantube <firmanhiday.id007@gmail.com>
2024-05-16 16:27:11 +0700

    add some css

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -4,8 +4,8 @@ import { formatIDR } from "../utils/formater";
 
 export default function CardProduct({product = {}, buttonText = "Edit", onClickBtnCard = () => {} }) {
     return (
-        <Card className=" rounded-0">
-            <Card.Img variant="top" src={product.storage_detail.secure_url} className=" rounded-0"/>
+        <Card className=" rounded-0 shadow-sm">
+            <Card.Img variant="top" src={product.storage_detail.secure_url} className=" rounded-0" style={{ height: "155px"}} />
             <Card.Body className=" text-center">
                 <Card.Title>{product.name}</Card.Title>
                 <Card.Text>{formatIDR(product.price)}</Card.Text>
--- src/components/admin/data-mobil/ListProduct.jsx
+++ src/components/admin/data-mobil/ListProduct.jsx
@@ -6,14 +6,14 @@ import CardProduct from "../../CardProduct";
 
 export default function ListProduct({dataProduct = []}) {
     const divStyle = {
-        marginTop: "2.875rem"}
+        margin: "2.875rem 0"}
     if (!dataProduct.length) return  <EmptyProduct />
 
     const onEditProduct = (product) => {
         console.log("ini", product)
     }
     return (
-        <div style={divStyle} className=" min-vh-100">
+        <div style={divStyle}>
             <Row>
                 {dataProduct.map((product, index) => (
                     <Col key={`card-product-${index + 1}`} lg="3" >
