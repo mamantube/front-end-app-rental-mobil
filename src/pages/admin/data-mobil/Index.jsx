ff6ce49cb20b3973f38f926bf0754788679671bd
mamantube <firmanhiday.id007@gmail.com>
2024-05-16 16:29:46 +0700

    implementation pagination button

--- src/pages/admin/data-mobil/Index.jsx
+++ src/pages/admin/data-mobil/Index.jsx
@@ -1,31 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import NavBreadcrumb from "../../../components/NavBreadcrumb";
 import DataMobilFilter from "../../../components/admin/data-mobil/DataMobilFilter";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 import useLoading from "../../../hooks/useLoading";
 import useAxios from "../../../hooks/useAxios";
 import ListProduct from "../../../components/admin/data-mobil/ListProduct";
-ListProduct
+import PaginationButton from "../../../components/PaginationButton";
+
+let navList = [
+    {
+        to:"/",
+        title: "Data Mobil",
+        isActive: true,
+    },
+]
 
 export default function DataMobil () {
     const navigateTo = useNavigate();
     const {showLoading, hideLoading} = useLoading();
+    const [load, setLoad] = useState(true)
 
     const [params, setParams] = useState(
         {
             q: "",
             sort_by: "",
             page: 1,
-            per_page: 20,
+            per_page: 1,
         }
     );
 
     const onChangeParams = (event) => {
         let { name, value} = event.target
-        setParams((params) => {
-            return {...params, [name]: value}
-        })
+        setParams({...params, [name]: value, page: 1})
     }
 
     const onSearch = () => {
@@ -33,43 +41,58 @@ export default function DataMobil () {
         console.log("THIS", params)
     }
 
+    const [totalPage, setTotalPage] = useState(0)
 
-    let navList = [
-        {
-            to:"/",
-            title: "Data Mobil",
-            isActive: true,
-        },
-    ]
+    const onPagePagination = (page) => {
+        console.log("ini", page, params)
+        setParams({...params, page})
+
+        setLoad(true)
+        searchProduct();
+        
+    }
 
     const axios = useAxios();
 
     const [ products, setProducts ] = useState([]);
 
+
     function searchProduct()  {
         showLoading(); 
+        
+        console.log("test", params)
 
         axios.get("api/v1/product", {params})
         .then((response) => {
             setProducts(response.data.data)
             console.log(response.data.data)
-        }).catch((error) => {
-            let messageError = error.response.data.message;
-            // let { message } = errors[0];
-            console.error("ERROR", messageError)
-            toast.error(messageError)
-        }).finally(() => {
-            hideLoading()
+
+            const { total } = response.data.pagination;
+
+            let resultTotalPage = Math.ceil(total/params.per_page)
+
+            setTotalPage(resultTotalPage)
+        })
+        // .catch((error) => {
+        //     let messageError = error.response.data.message;
+        //     // let { message } = errors[0];
+        //     console.error("ERROR", messageError)
+        //     toast.error(messageError)
+        // })
+        .finally(() => {
+            hideLoading();
         })
     }
 
     useEffect(() => {
-        searchProduct()
+        if (load) searchProduct()
 
         return () => {
+            setLoad(false)
             setProducts([])
+
         }
-    }, [])
+    }, [load])
 
 
     return (
@@ -79,6 +102,8 @@ export default function DataMobil () {
             <DataMobilFilter q={params.q} sort_by={params.sort_by} onChangeValue={onChangeParams} onClickSearch={onSearch} onCreateNew={() => navigateTo("/admin/data-mobil/buat-baru")} />
 
             <ListProduct dataProduct={products} />
+
+            <PaginationButton dataProduct={products} currentPage={params.page} onPage={onPagePagination} totalPage={totalPage} />
         </section>
     )
 }
