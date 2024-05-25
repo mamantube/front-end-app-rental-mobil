1f1bce3022842a46064961fcf0863031da44a910
mamantube <firmanhiday.id007@gmail.com>
2024-05-25 19:26:56 +0700

    add maintenance text message on product card

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -13,15 +13,34 @@ export default function CardProduct(props) {
     objectFit: "contain",
     aspectRatio: "1/1",
     objectPosition: "center",
+    opacity: product.deleted_at ? "0.4" : "1"
   };
-  return (
-    <Card className=" rounded-0 h-100 shadow">
+
+  let element
+
+  if (product && product.deleted_at) {
+    element = (
+      <div className=" position-relative d-flex justify-content-center align-items-center">
+        <Card.Text className=" position-absolute">
+            Kendaraan dalam perbaikan
+        </Card.Text>
+        <Card.Img variant="top" src={product.storage_detail.secure_url} style={styleImg} />
+      </div>
+    );
+  } else {
+    element = (
       <Card.Img
         variant="top"
         src={product.storage_detail.secure_url}
         className=" rounded-0"
         style={styleImg}
       />
+    )
+  }
+  return (
+    <Card className=" rounded-0 h-100 shadow">
+      {element}
+      
       <Card.Body className=" text-center">
         <Card.Title>{product.name}</Card.Title>
         <Card.Text>{formatIDR(product.price)}</Card.Text>
