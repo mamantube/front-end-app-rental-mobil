26dbd6274facfffe7e244b5b737ed5bd242997c7
mamantube <firmanhiday.id007@gmail.com>
2026-07-28 09:38:10 +0700

    update navbar to tailwindcss

--- src/layouts/Landing.jsx
+++ src/layouts/Landing.jsx
@@ -1,7 +1,7 @@
 import {
   Outlet,
   NavLink,
-  // useNavigate
+  useNavigate
 } from "react-router-dom";
 // import useLoading from "../hooks/useLoading";
 // import { Navbar, Button, Nav, Container } from "react-bootstrap";
@@ -11,7 +11,7 @@ import { useState } from "react";
 
 export default function LayoutLanding() {
   const [show, setShow] = useState(false);
-  // const navigateTo = useNavigate();
+  const navigateTo = useNavigate();
 
   // let cssShowMenu = show ? "d-block" : "d-none";
 
@@ -23,9 +23,9 @@ export default function LayoutLanding() {
   //   navigateTo("/register");
   // }
 
-  // function onToLogin() {
-  //   navigateTo("/login");
-  // }
+  function onToLogin() {
+    navigateTo("/login");
+  }
 
   // const { isLoading } = useLoading();
 
@@ -34,16 +34,21 @@ export default function LayoutLanding() {
 
   return (
     <header>
-      <nav className="flex justify-between bg-gray-700 py-4 px-4">
-        <div>
-          <h1 className="text-white text-2xl font-bold">
+      <nav className="sticky flex justify-between bg-white py-4 px-6 items-center shadow-sm">
+        <div className="flex gap-4 items-center">
+          <h1 className="text-gray-800 text-2xl font-bold">
             <NavLink to="/">MAREMO</NavLink>
           </h1>
+          <div className="hidden gap-4 text-gray-800 font-medium lg:flex">
+            <NavLink className="transition ease-in-out duration-200 hover:scale-110 active:font-semibold">Beranda</NavLink>
+            <NavLink className="transition  ease-in-out duration-200 hover:scale-110 active:font-semibold">Rental Mobil</NavLink>
+          </div>
         </div>
 
-        <div className="hidden gap-4 text-white font-medium lg:flex">
-          <NavLink>Beranda</NavLink>
-          <NavLink>Rental Mobil</NavLink>
+        <div className="hidden gap-4 text-gray-800 font-medium lg:flex">
+          <button className="cursor-pointer rounded-lg border border-gray-800 px-4 hover:scale-110 duration-500 transition hover:bg-gray-800 hover:text-white " onClick={onToLogin} >
+            Book Now
+          </button>
         </div>
 
         <div className="flex items-center lg:hidden">
@@ -72,14 +77,23 @@ export default function LayoutLanding() {
             <span className="sr-only">Toggle Menu</span>
           </button>
         </div>
-
       </nav>
-        <div id="mobile-menu" className={`overflow-hidden bg-gray-700 transition-all duration-300 lg:hidden ${show ? "block" : "hidden"}`}>
-          <div className="flex flex-col gap-2 p-4">
-            <NavLink>Beranda</NavLink>
-            <NavLink>Rental Mobil</NavLink>
-          </div>
+      <div
+        id="mobile-menu"
+        className={`overflow-hidden bg-gray-700 transition-all duration-300 lg:hidden ${show ? "block" : "hidden"}`}
+      >
+        <div className="flex flex-col gap-2 p-4">
+          <NavLink>Beranda</NavLink>
+          <NavLink>Rental Mobil</NavLink>
+          <button className="cursor-pointer font-semibold overflow-hidden relative z-100 border rounded-md border-green-500 group px-2 bg-white" onClick={onToLogin}>
+            <span className="relative z-10 text-gray-700 group-hover:text-white text-sm duration-500">
+              Book Now
+            </span>
+            <span className="absolute w-full h-full bg-[#364153] -left-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:left-0 duration-500"></span>
+            <span className="absolute w-full h-full bg-[#1E293B] -right-32 top-0 -rotate-45 group-hover:rotate-0 group-hover:right-0 duration-500"></span>
+          </button>
         </div>
+      </div>
 
       {/* {componentLoading} */}
       {/* <nav
