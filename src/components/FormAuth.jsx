7bd79e7db9f5144784f6ce7fbcbe57e1b21fe2d0
mamantube <firmanhiday.id007@gmail.com>
2026-07-31 18:08:05 +0700

    migration FormAuth to Tailwindcss

--- src/components/FormAuth.jsx
+++ src/components/FormAuth.jsx
@@ -1,3 +1,22 @@
+export default function FormAuth( props ) {
+    let { children, title = "judul", subTitle = "sub judul"} = props;
+
+    return (
+        <div className="card w-96 bg-base-100 shadow-lg border border-gray-700">
+            <div className="card-body bg-amber-50 rounded-lg">
+                <div className="card-title text-center text-gray-700 text-xl">
+                    {title}
+                </div>
+                <div className="text-xs text-gray-700">
+                    {subTitle}
+                </div>
+                {children}
+            </div>
+        </div>
+    )
+
+}
+
 // /* eslint-disable react/prop-types */
 // import { Card } from "react-bootstrap";
 // import "../assets/CSS/FormAuth.css";
