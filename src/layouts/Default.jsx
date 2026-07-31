ca64e992841ad89a26fcf32266185a106455db1e
mamantube <firmanhiday.id007@gmail.com>
2026-07-31 18:09:27 +0700

    migration default layout to tailwindcss

--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -1,3 +1,28 @@
+import { Outlet } from "react-router-dom";
+import useLoading from "../hooks/useLoading";
+import { ToastContainer } from "react-toastify";
+import Loading from "../components/Loading";
+
+
+export default function LayoutDefault() {
+    const { isLoading } = useLoading();
+
+    let loadingComponent;
+
+    if (isLoading) loadingComponent = <Loading />
+    return (
+        <>
+            <ToastContainer position="top-right" />
+                {loadingComponent}
+
+            <div className=" bg-white">
+                <Outlet key="layout-default" />
+            </div>
+        </>
+    )
+}
+
+
 // import { Outlet } from "react-router-dom";
 // import Loading from "../components/Loading";
 // import useLoading from "../hooks/useLoading";
