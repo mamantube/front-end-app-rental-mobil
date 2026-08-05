7eb6d04068fed2c77db17f68540f50fc86a92031
mamantube <firmanhiday.id007@gmail.com>
2026-08-05 14:17:13 +0700

    migration to tailwindcss

--- src/pages/ForbiddenAcces.jsx
+++ src/pages/ForbiddenAcces.jsx
@@ -1,34 +1,34 @@
 // import { Button } from "react-bootstrap";
-// import { useNavigate } from "react-router-dom";
-// import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-// export default function ForbiddenAccess() {
-//   const navigateTo = useNavigate();
-//   const dispatch = useDispatch();
+export default function ForbiddenAccess() {
+  const navigateTo = useNavigate();
+  const dispatch = useDispatch();
 
-//   function toLanding() {
-//     localStorage.clear();
+  function toLanding() {
+    localStorage.clear();
 
-//     dispatch({ type: "SET_TOKEN", value: null });
-//     dispatch({ type: "SET_ROLE", value: null });
-//     dispatch({ type: "SET_USER_ID", value: null })
-//     return navigateTo("/");
-//   }
+    dispatch({ type: "SET_TOKEN", value: null });
+    dispatch({ type: "SET_ROLE", value: null });
+    dispatch({ type: "SET_USER_ID", value: null })
+    return navigateTo("/");
+  }
 
-//   return (
-//     <main className=" d-flex justify-content-center align-items-center min-vh-100">
-//       <div className=" text-center">
-//         <img src="/img/forbidden.png" alt="" className=" mb-4" />
-//         <h1> 403 | Forbidden Access</h1>
-//         <p className=" text-h5 mt-5">
-//           Maff akses ditolak. Anda tidak memiliki izin untuk mengakses halaman
-//           ini
-//         </p>
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <div className=" text-center">
+        <img src="/img/forbidden.png" alt="" className=" mb-4" />
+        <h1> 403 | Forbidden Access</h1>
+        <p className=" text-h5 mt-5">
+          Maff akses ditolak. Anda tidak memiliki izin untuk mengakses halaman
+          ini
+        </p>
 
-//         <Button variant="dark" className=" rounded-0 mt-4" onClick={toLanding}>
-//           Ke halaman utama
-//         </Button>
-//       </div>
-//     </main>
-//   );
-// }
+        <button className=" rounded-0 mt-4" onClick={toLanding}>
+          Ke halaman utama
+        </button>
+      </div>
+    </main>
+  );
+}
