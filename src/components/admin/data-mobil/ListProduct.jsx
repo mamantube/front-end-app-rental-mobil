9c2c749a7f10d7f3f7611870670db839bf518d29
mamantube <firmanhiday.id007@gmail.com>
2026-09-11 12:58:37 +0700

    migration list product on admin panel to tailwind css

--- src/components/admin/data-mobil/ListProduct.jsx
+++ src/components/admin/data-mobil/ListProduct.jsx
@@ -1,58 +1,39 @@
+/* eslint-disable react/prop-types */
 import EmptyProduct from "../../EmptyProduct";
 import { useNavigate } from "react-router-dom";
 import CardProduct from "../../CardProduct";
 
-export default function ListProduct( dataProduct = []) {
-    const navigateTo = useNavigate();
+export default function ListProduct({ dataProduct = [] }) {
+  const navigateTo = useNavigate();
 
-    const goToEdit = (_id) => {
-        navigateTo(`/admin/data-mobil/edit/${_id}`)
-    }
+  const goToEdit = (_id) => {
+    navigateTo(`/admin/data-mobil/edit/${_id}`);
+  };
 
-    if (!dataProduct.length) return <EmptyProduct />;
+  if (!dataProduct.length) {
+    return <EmptyProduct />;
+  }
 
-    return (
-        <section>
-            {dataProduct.map((detailProduct, index) => (
-                <CardProduct
-                    key={`card-product-${index + 1}`}
-                    product={detailProduct}
-                    onClickBtnCard={() => goToEdit(detailProduct._id)}
-                />
-            ))}
-        </section>
-    )
+  return (
+    <section className="mx-auto max-w-7xl px-4 py-6">
+      <div
+        className="
+          grid
+          grid-cols-1
+          gap-5
+          sm:grid-cols-2
+          lg:grid-cols-3
+          xl:grid-cols-4
+        "
+      >
+        {dataProduct.map((detailProduct, index) => (
+          <CardProduct
+            key={`card-product-${index + 1}`}
+            product={detailProduct}
+            onClickBtnCard={() => goToEdit(detailProduct._id)}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
-
-// /* eslint-disable react/prop-types */
-// import { Row, Col } from "react-bootstrap";
-// import EmptyProduct from "../../EmptyProduct";
-// import CardProduct from "../../CardProduct";
-// import { useNavigate } from "react-router-dom";
-
-// export default function ListProduct({ dataProduct = [] }) {
-//   const navigateTo = useNavigate();
-
-//   function goToEdit(_id) {
-//     navigateTo(`/admin/data-mobil/edit/${_id}`)
-//   }
-
-//   const divStyle = {
-//     margin: "2.875rem 0",
-//   };
-//   if (!dataProduct.length) return <EmptyProduct />;
-//   return (
-//     <div style={divStyle}>
-//       <Row className=" g-3">
-//         {dataProduct.map((detailProduct, index) => (
-//           <Col key={`card-product-${index + 1}`} lg="3">
-//             <CardProduct
-//               product={detailProduct}
-//               onClickBtnCard={() => goToEdit(detailProduct._id) }
-//             />
-//           </Col>
-//         ))}
-//       </Row>
-//     </div>
-//   );
-// }
