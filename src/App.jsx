aead98d3fd9206b0dd5a2e596c93943062a9f3f9
mamantube <firmanhiday.id007@gmail.com>
2026-09-15 15:09:58 +0700

    import buat baru

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Beranda from "./pages/Beranda";
 import RentalMobil from "./pages/RentalMobil";
 import LayoutDashboard from "./layouts/Dashboard";
 import DataMobil from "./pages/admin/data-mobil/Index";
-// import BuatBaru from "./pages/admin/data-mobil/BuatBaru";
+import BuatBaru from "./pages/admin/data-mobil/BuatBaru";
 // import DataPengguna from "./pages/admin/data-pengguna";
 import DataTransaksi from "./pages/admin/data-transaksi";
 import Login from "./pages/Login";
@@ -29,8 +29,8 @@ export default function App() {
           <Route index path="data-transaksi" Component={DataTransaksi} />
           <Route path="data-mobil">
             <Route index Component={DataMobil} />
-            {/* <Route path="buat-baru" Component={BuatBaru} />
-            <Route path="edit/:product_id" Component={EditDataMobil} /> */}
+            <Route path="buat-baru" Component={BuatBaru} />
+            {/* <Route path="edit/:product_id" Component={EditDataMobil} /> */}
           </Route>
 
           {/* <Route path="data-pengguna" Component={DataPengguna} /> */}
